0f73594e0f744d1517850dedce145f0ae07dcd50
Maddie752 <madison.davis@patelhs.org>
2020-10-16 17:54:23 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,66 @@
+import java.util.Scanner;
+
+public class ScannerTut {
+  public static void main(String[] args) {
+    System.out.println("wellcome to the art gallery!");
+    System.out.println("how many pices of art would you like to buy");
+      
+       Scanner scan = new Scanner(System.in);
+      int picesOfArt = scan.nextInt();
+      int count = 1;
+    while (count < picesOfArt) {
+      System.out.print("witch pices do you want to buy");
+      int witchPices = scan.nextInt();
+
+     if (witchPices == 2){
+
+    }
+    else 
+    {
+
+    System.out.println("that's a good choice");
+    System.out.println("would you like your pices to be gift wraped up for you ");
+    System.out.println("or put in a bag ");  
+    System.out.println(" have a good day and come again");
+    //
+    public static void main(String[] args) {
+    System.out.println("wellcome to the art gallery!");
+    System.out.println("how many pices of art would you like to buy");
+      
+       Scanner scan = new Scanner(System.in);
+      int picesOfArt = scan.nextInt();
+      int count = 3;
+    while (count < picesOfArt) {
+      System.out.print("witch pices do you want to buy");
+      int witchPices = scan.nextInt();
+
+     if (witchPices == 4){
+    }
+     if (witchPices == 5){
+
+    }
+    else 
+    {
+
+    System.out.println("those are also a very good choices");
+    System.out.println("how would you like your pices to be delivered for you");
+    System.out.println("we can do it by airplane, overseas,or by mail");  
+     System.out.println("by mail you said");
+     byMailYouSaid = scan.nextInt();
+     System.out.println("will that be all");
+     willThatBeAll = scan.nextInt();
+     System.out.println(" have a good day");
+    
+    
+    
+   
+   
+    }
+  
+
+  
+
+    }
+    }
+  }
+}
